6b1cbf35e3cde9c8d43f9061bb9e9fad0717b158
Erik Barriga <erik.barriga@teamsparq.com>
2026-04-16 16:24:37 -0600

    Remove Weather (dummy code)

--- EB.FeatureFlag.Aspire.Web/Program.cs
+++ EB.FeatureFlag.Aspire.Web/Program.cs
@@ -26,13 +26,6 @@ builder.Services.AddFeatureFlagWebAuth(options =>
 
 builder.Services.AddCascadingAuthenticationState();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
-
 builder.Services.AddHttpClient<FeatureFlagApiClient>(client =>
     {
         client.BaseAddress = new("https+http://apiservice");
